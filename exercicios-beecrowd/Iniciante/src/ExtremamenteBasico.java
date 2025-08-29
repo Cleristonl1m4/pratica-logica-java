04151c6706b36d1299056b9a870b9fe9a2a02f50
Cleristonl1m4 <cleristonlima180600@gmail.com>
2025-08-29 14:11:10 -0300

    improving the organization of the structure and adding one more basic exercise.

---
+++ exercicios-beecrowd/Iniciante/src/ExtremamenteBasico.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExtremamenteBasico {
+    public static void main(String[] args) {
+        int x = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 2; i++){
+            x += sc.nextInt();
+        }
+        System.out.println("X = "+x);
+    }
+}
